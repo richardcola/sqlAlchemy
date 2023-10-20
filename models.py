729653d6032df5959abd08d757fc79e37dd4a6f3
Richard Colaianni <44618480+richardcola@users.noreply.github.com>
2023-10-20 09:20:27 -0400

    Add files via upload

---
+++ models.py
@@ -0,0 +1,34 @@
+"""SQLAlchemy blogly models"""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+DEFAULT_IMAGE_URL = "https://www.freeiconspng.com/uploads/icon-user-blue-symbol-people-person-generic--public-domain--21.png"
+
+
+class User(db.Model):
+    """this is Site user."""
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.Text, nullable=False)
+    last_name = db.Column(db.Text, nullable=False)
+    image_url = db.Column(db.Text, nullable=False, default=DEFAULT_IMAGE_URL)
+
+    @property
+    def full_name(self):
+        """user's FN"""
+
+        return f"{self.first_name} {self.last_name}"
+
+
+def connect_db(app):
+    """Connects DB to Flask app.
+
+    called  into Flask app.
+    """
+
+    db.app = app
+    db.init_app(app)
